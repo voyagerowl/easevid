ac89940035dad39636c2cab4008c003190c5300d
voyagerowl <113039549+voyagerowl@users.noreply.github.com>
2022-09-08 10:01:09 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -15,5 +15,5 @@ convertBtn.addEventListener('click', () => {
       return true; 
     } 
 function sendURL(URL){
-window.location.href = `http://localhost:4000/download?URL=${URL}`;
+window.location.href = `https://www.invincible-amplified-physician.glitch.me/download?URL=${URL}`;
 }
